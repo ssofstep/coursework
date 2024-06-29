42076a705a9f1b82701c515b2272aa62e0605684
sofia stepanova <you@example.com>
2024-06-29 18:20:16 +0300

    add something

--- src/reports.py
+++ src/reports.py
@@ -1,27 +1,30 @@
 from datetime import datetime, timedelta
 from typing import Optional
-from src.utils import suitable_transactions, read_xls_file
+
 import pandas as pd
 
+from src.decorators import reports
 from src.logger import setup_logger
 
 logger = setup_logger("reports", "reports.log")
 
 
-def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+@reports()
+def spending_by_category(
+    transactions: pd.DataFrame, category: str, date: Optional[str] = None
+) -> pd.DataFrame:
     """Функция, которая возвращает траты по заданной категории за последние три месяца (от переданной даты)."""
     if date is None:
         date_time = datetime.now()
     else:
         date_time = datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
-    starting_date = date_time - timedelta(days=90)
-    selected_transactions = suitable_transactions(transactions, category)
-    spending_transactions = []
-    for i in selected_transactions:
-        if datetime.strptime(i["Дата операции"], "%d.%m.%Y %H:%M:%S") >= starting_date:
-            spending_transactions.append(i)
-    return spending_transactions
-
-
-rd = read_xls_file("C:\\Users\\Sonya\\lecture\\coursework\\data\\operations.xls")
-print(spending_by_category(rd, "Переводы", "07.06.2007 16:44:00"))
+    transactions = transactions[transactions["Сумма операции"] < 0]
+    transactions = transactions[transactions["Категория"] == category]
+    end_date = date_time - timedelta(days=90)
+    transactions = transactions[
+        pd.to_datetime(transactions["Дата операции"], dayfirst=True) <= date_time
+    ]
+    transactions = transactions[
+        pd.to_datetime(transactions["Дата операции"], dayfirst=True) > end_date
+    ]
+    return pd.DataFrame(transactions)
