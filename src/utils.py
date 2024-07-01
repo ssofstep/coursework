866e5d277903021cec77e535923182289d660b52
sofia stepanova <you@example.com>
2024-07-01 21:11:20 +0300

    add functions

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, time
 from typing import Any
 
 import pandas as pd
@@ -28,3 +29,45 @@ def suitable_transactions(xls_file: list[dict], search_line: str) -> Any:
             new_list.append(transaction)
     logger.info(f"found dictionaries by word {search_line}")
     return new_list
+
+
+def greeting_message(date: str):
+    """Функция, которая возвращает приветствие в зависимости от времени"""
+    logger.info("Подбирает приветствие")
+    date_time = datetime.strptime(date, "%d.%m.%Y %H:%M:%S").time()
+    greeting_time = {
+        "Доброй ночи": time(6, 0, 0),
+        "Доброе утро": time(12, 0, 0),
+        "Добрый день": time(17, 0, 0),
+        "Добрый вечер": time(23, 0, 0),
+    }
+
+    for k, v in greeting_time.items():
+        if date_time <= v:
+            logger.info(f"Приветствие {k}")
+            return k
+
+
+def each_card(transactions: list[dict]) -> list[dict]:
+    dict_summ: dict = {}
+    dict_cashback: dict = {}
+    for i in transactions:
+        if isinstance(i["Номер карты"], str) and i["Сумма операции"] < 0:
+            dict_summ[i["Номер карты"]] = (
+                dict_summ.get(i["Номер карты"], 0) + i["Сумма операции"] * -1
+            )
+            if i["Сумма операции"] * -1 > 100:
+                dict_cashback[i["Номер карты"]] = (
+                    dict_cashback.get(i["Номер карты"], 0)
+                    + (i["Сумма операции"] * -1) / 100
+                )
+    list_card = []
+    for k, v in dict_summ.items():
+        list_card.append(
+            {
+                "last_digit:": k[-4:],
+                "total_spent:": v,
+                "cashback:": dict_cashback.get(k, 0),
+            }
+        )
+    return list_card
